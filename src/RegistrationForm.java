cfde69b625776f09cc687528de3bdbf443bf577f
viraj <viraj123e@gmail.com>
2023-08-02 15:04:31 +0530

    add if statement for isEmpty and to check password and comformpassword

--- src/RegistrationForm.java
+++ src/RegistrationForm.java
@@ -38,6 +38,29 @@ public class RegistrationForm extends JDialog{
     }
 
     private void registerUser() {
+        String name=Name.getText();
+        String email=Email.getText();
+        String phone=Phone.getText();
+        String address=Address.getText();
+        String password=String.valueOf(Password.getPassword());
+        String comformPassword=String.valueOf(ComformPassword.getPassword());
+
+        if (name.isEmpty()||email.isEmpty()||phone.isEmpty()||address.isEmpty()||password.isEmpty()){
+            JOptionPane.showMessageDialog(this,
+                    "please enter all field",
+                    "Try again",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        if(!password.equals(comformPassword)){
+            JOptionPane.showMessageDialog(this,
+                    "Confirm Password does not match",
+                            "Try again",
+                             JOptionPane.ERROR_MESSAGE);
+            return;
+
+        }
     }
 
     public static void main(String[] args) {
